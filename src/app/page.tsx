e13beadb23324b75a8146c666f461a2c4037f554
cesar lahoud <cesar-lahoud@hotmail.com>
2025-03-02 23:41:17 -0500

    removed logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { NavBar } from "@/components/NavBar";
 import ContactSection from "@/components/section/ContactSection";
 import FooterSection from "@/components/section/FooterSection";
 import HeroSection from "@/components/section/HeroSection";
+// import ProjectSection from "@/components/section/ProjectSection";
 import ResumeSection from "@/components/section/ResumeSection";
+// import { SkillsSection } from "@/components/section/SkillsSection";
 
 export default function Home() {
 	return (
@@ -10,6 +12,8 @@ export default function Home() {
 			<NavBar />
 			<HeroSection />
 			<ResumeSection />
+			{/* <ProjectSection /> */}
+			{/* <SkillsSection /> */}
 			<ContactSection />
 			<FooterSection />
 		</>
