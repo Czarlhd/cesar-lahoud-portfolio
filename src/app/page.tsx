7162d0a2a3c2e6138169e930a35a538b0093e48b
Czarlhd <cesar-lahoud@hotmail.com>
2026-02-19 21:36:42 -0500

    removed old portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import { NavBar } from "@/components/NavBar";
-import ContactSection from "@/components/section/ContactSection";
-import FooterSection from "@/components/section/FooterSection";
-import HeroSection from "@/components/section/HeroSection";
-// import ProjectSection from "@/components/section/ProjectSection";
-import ResumeSection from "@/components/section/ResumeSection";
-// import { SkillsSection } from "@/components/section/SkillsSection";
-
 export default function Home() {
-	return (
-		<>
-			<NavBar />
-			<HeroSection />
-			<ResumeSection />
-			{/* <ProjectSection /> */}
-			{/* <SkillsSection /> */}
-			<ContactSection />
-			<FooterSection />
-		</>
-	);
+	return <div> CV</div>;
 }
